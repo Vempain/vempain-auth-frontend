e5debabe082fa13d653d0e2602c123ceb7ab9d9f
Paul-Erik Törrönen <poltsi@poltsi.fi>
2025-09-20 19:19:40 +0300

    Use Dayjs in AbstractResponse.ts for datetime fields
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- src/models/Responses/AbstractResponse.ts
+++ src/models/Responses/AbstractResponse.ts
@@ -1,11 +1,12 @@
 import type {AclVO} from "./AclVO.ts";
+import type {Dayjs} from "dayjs";
 
 export interface AbstractResponse {
     id: number;
     acls: AclVO[];
     locked: boolean;
-    creator: number | null;
+    creator: number;
+    created: Dayjs;
     modifier: number | null;
-    created: string;
-    modified: string;
+    modified: Dayjs | null;
 }
